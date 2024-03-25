8617e0f78602e95ffe7ab38675da1153131cad94
jie.zhouleblond <jie.zhouleblond@hardis-group.com>
2024-03-25 11:44:42 +0100

    feat: delete multipes users

--- pages/1_users.py
+++ pages/1_users.py
@@ -1,58 +1,52 @@
 import streamlit as st
 
 from menu import menu
-from users_table import load_user_data, show_df
-# from users_table import UsersTable 
+from users_table import show_df, delete_users, new_user, modify_user
 
 st.set_page_config(page_title="Users", layout="wide", initial_sidebar_state="auto")
 
 menu()
 
-
-
-
-
+def switch_delete_button():
+    st.session_state.delete_clicked = not st.session_state.delete_clicked
 
 if "snow_connector" not in st.session_state:
     st.warning("Not connected to snowflake")
 else:
-    if "one_user_selected" not in st.session_state:
-        st.session_state.one_user_selected = False
-
-
     tab1, tab2 = st.tabs([" 📋Users List ", "  ➕New User "])
 
     with tab1:
+        if "nb_selected" not in st.session_state:
+            st.session_state.nb_selected = 0
+        if "delete_clicked" not in st.session_state:
+            st.session_state.delete_clicked = False
+            
+        show_df()
 
-        show_df()   
-        
-        
-        # if "users_table" not in st.session_state:
-        #     st.session_state.users_table = UsersTable()
-        # users_table = st.session_state.users_table
-        # users_table.show_table()
-        
-        
-        # if st.session_state.one_user_selected is True:
-        # if users_table.active_buttons is True:
-        #     left, right = st.columns([0.3, 0.7])
-        #     with left:
-        #         delete_clicked = st.button("Delete", type="primary")
-        #         if delete_clicked:
-        #             users_table.delete_user()
-        #     with right:
-        #         st.button("Modify", type="primary")
-            # option = st.selectbox(
-            #     "Select one user to modify or multipe users to delete",
-            #     ("Modify", "Delete"),
-            #     index=None,
-            #     placeholder="Select your action...",
-            # )
-            # if option == "Delete":
-            #     users_table.delete_user()
-            # if option == "Modify":
-            #     users_table.modify_user()                
-
+        # nb_selected is updated in the show_df function
+        if st.session_state.nb_selected > 0:
+            col_modif, col_delete = st.columns([0.3, 0.7])
+            with col_modif:
+                modified = st.button(
+                    "Modify",
+                    help="select one user to modify",
+                    type="primary",
+                    disabled=st.session_state.nb_selected != 1,
+                )
+                if modified:
+                    modify_user()
+            with col_delete:
+                st.button("Delete", type="primary", on_click=switch_delete_button)
+                if st.session_state.delete_clicked:
+                    selected_rows = st.session_state.df_buffer[st.session_state.df_buffer["Action"]]
+                    st.warning("Do you confirm the deletion of the following users?")
+                    st.write(selected_rows)
+                    col_confirm, col_cancel = st.columns([0.1, 0.5])
+                    with col_confirm:
+                        if st.button("Confirm", type="primary"):
+                            delete_users(selected_rows)
+                    with col_cancel:
+                        st.button("Cancel", type="secondary", on_click=switch_delete_button)
 
     with tab2:
         st.header("Create a new user")
@@ -67,7 +61,6 @@ else:
             submitted = st.form_submit_button("Submit", type="primary")
             if submitted:
                 if password1 == password2:
-                    # create_newuser(login, first_name, last_name, password1)
-                    users_table.new_user(login, first_name, last_name, password1)
+                    new_user(login, first_name, last_name, password1)
 
         # st.button("Cancel", on_click=switch_newuser_button)
--- users_table.py
+++ users_table.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import snowflake.connector
 import streamlit as st
+import time
 
 
 @st.cache_data
@@ -8,7 +8,6 @@ def load_user_data():
     cur = st.session_state.snow_connector.cursor()
     try:
         cur.execute("SHOW USERS")
-
         cur.execute(
             """
             select "name", "login_name", "first_name", "last_name", "email"
@@ -35,6 +34,15 @@ def filter_df(df, text_search):
     return filtered_df
 
 
+def update_nb_selected_in_session_state():
+    """
+    check if the user selected at least one user
+    """
+
+    df = st.session_state.df_buffer
+    st.session_state.nb_selected = len(df[df["Action"]])
+
+
 def save_selection_in_buffer():
     """
     user clicked one selectbox,
@@ -50,33 +58,33 @@ def save_selection_in_buffer():
         st.session_state.df_buffer.loc[
             st.session_state.df_buffer["name"] == name, "Action"
         ] = value["Action"]
+    update_nb_selected_in_session_state()
 
 
 def show_df():
     """
     if user search for a user, then show filtered dataframe + selected users
     else show the original dataframe with selected users
-    
+
     !!!important information:
     - df_view is the dataframe that the user see
     - df_buffer is the dataframe which store the selections
     why this complexity? because streamlit has a specific way to handle dataframes
     each time we click on a selectbox, the application rerun from the top, but on the frond end
-    it doesn't always reload the dataframe to avoid flashing effect, 
+    it doesn't always reload the dataframe to avoid flashing effect,
     it will reload when the parameters of st.data_editor change.
     So If we change the value of df_view each time we click, the page will flash each time we click.
-    To avoid this, we use df_buffer to store the selections, 
+    To avoid this, we use df_buffer to store the selections,
     and we update the df_view only when we use the filter or when we take off the filer
-    
+
     """
 
     if "df_view" not in st.session_state:
-        # To do later : check if user reload when I clear cache
         st.session_state.df_view = load_user_data()
+        st.session_state.df_buffer = load_user_data()
+
     if "last_search" not in st.session_state:
         st.session_state.last_search = ""
-    if "df_buffer" not in st.session_state:
-        st.session_state.df_buffer = load_user_data()
 
     text_search = st.text_input("🔍")
 
@@ -84,16 +92,16 @@ def show_df():
     if text_search and text_search != st.session_state.last_search:
         df = st.session_state.df_buffer.copy(deep=True)
         selected_rows = df[df["Action"]]
-        
+
         # take off selected rows from the dataframe to avoid duplicates
         df = df.drop(selected_rows.index)
 
         df = filter_df(df, text_search)
-        
+
         # combine selected rows with the filtered dataframe
         df = pd.concat([df, selected_rows])
-        
-        # update the view 
+
+        # update the view
         st.session_state.df_view = df
 
     # delete the filter, so update the view
@@ -110,104 +118,70 @@ def show_df():
     )
 
 
-def user_selected():
-    st.session_state.one_user_selected = True
-
-
-class UsersTable:
-    def __init__(self) -> None:
-        self.cur = st.session_state.snow_connector.cursor()
-        self.df = load_user_data()
-
-    def filter_table(self, text_search):
-        df = self.df
-        m1 = df["name"].str.contains(text_search, case=False)
-        m2 = df["login_name"].str.contains(text_search, case=False)
-        m3 = df["first_name"].str.contains(text_search, case=False)
-        m4 = df["last_name"].str.contains(text_search, case=False)
-        m5 = df["email"].str.contains(text_search, case=False)
-        filtered_df = df[m1 | m2 | m3 | m4 | m5]
-        return filtered_df
-
-    def show_table(self):
-        text_search = st.text_input("🔍")
-        if text_search:
-            filtered_df = self.filter_table(text_search)
-            show = filtered_df
-            selected_row = self.df[self.df["Action"]]
-            st.write(selected_row)
-        else:
-            show = self.df
-
-        # selected = self.df[self.df["Action"]]
-
-        self.df = st.data_editor(
-            show, key="users_list", column_config={"name": "user name"}, hide_index=True
+def new_user(login, first_name, last_name, password):
+    cur = st.session_state.snow_connector.cursor()
+    try:
+        cur.execute(
+            f"""CREATE USER "{login}" PASSWORD="{password}" FIRST_NAME="{first_name}" LAST_NAME="{last_name}"
+            """
         )
+    except Exception as e:
+        st.warning(e)
+    else:
+        st.success(cur.fetchone()[0], icon="✅")
+        # clear cache to make sure new created user is displayed
+        load_user_data.clear()
+        del st.session_state["df_view"]
+    finally:
+        cur.close()
+        time.sleep(2)
+        st.rerun()
 
-    def active_buttons(self):
-        self.show_delete_btton = True
 
-    def new_user(self, login, first_name, last_name, password):
-        try:
-            self.cur.execute(
-                f"""CREATE USER "{login}" PASSWORD="{password}" FIRST_NAME="{first_name}" LAST_NAME="{last_name}"
-                """
-            )
-        except snowflake.connector.errors.ProgrammingError as e:
-            st.error(e)
-        except Exception as e:
-            st.warning(e)
-        else:
-            st.success(self.cur.fetchone()[0])
-            load_user_data.clear()
-        finally:
-            self.cur.close()
-
-    def delete_user(self):
-        selected_rows = self.df[self.df.iloc[:, 3]]
+def delete_users(selected_rows):
+    messages = []
+    try:
         for _index, row in selected_rows.iterrows():
-            name, first_name, last_name, _selection = row
-            st.warning(f"Do you confirm the deletion of {first_name} {last_name}")
-            if st.button("Confirm", type="primary"):
-                cur = st.session_state.snow_connector.cursor()
-                try:
-                    cur.execute(f"DROP USER {name}")
-                except snowflake.connector.errors.ProgrammingError as e:
-                    st.write(e)
-                except Exception as e:
-                    st.warning(e)
-                else:
-                    st.success(cur.fetchone()[0])
-                    load_user_data.clear()
-                    st.rerun()
-                finally:
-                    cur.close()
+            name = row["name"]
+            cur = st.session_state.snow_connector.cursor()
+            cur.execute(
+                f"""DROP USER "{name}"
+                        """
+            )
+            messages.append(cur.fetchone()[0])
+    except Exception as e:
+        st.warning(e)
+    else:
+        for msg in messages:
+            st.success(msg, icon="✅")
+        st.session_state.delete_clicked = False
+    finally:
+        cur.close()
+        load_user_data.clear()
+        del st.session_state["df_view"]
+        time.sleep(2)
+        st.rerun()
 
-    def modify_user(self):
-        selected_rows = self.df[self.df.iloc[:, 3]]
-        if len(selected_rows) != 1:
-            st.error("Select ONE user only, Please recheck your selection")
-        else:
-            name, first, last, _ = selected_rows.values[0]
+def modify_user():
+    df = st.session_state.df_buffer
+    selected_rows = df[df.iloc[:, 3]]
+    name, first, last, _ = selected_rows.values[0]
 
-            change_password = st.toggle(
-                f"I want to change user {first} {last}'S password"
+    change_password = st.toggle(f"change user {first} {last}'S password")
+
+    with st.form("modify_user"):
+        st.text_input("login", value=name, disabled=True)
+        m_first_name = st.text_input("First name", value=first)
+        m_last_name = st.text_input("last name", value=last)
+        if change_password:
+            password1 = st.text_input("new password", type="password")
+            password2 = st.text_input(
+                "confirm password", type="password", disabled=True
             )
 
-            with st.form("modify_user"):
-                st.text_input("login", value=name, disabled=True)
-                m_first_name = st.text_input("First name", value=first)
-                m_last_name = st.text_input("last name", value=last)
-                if change_password:
-                    password1 = st.text_input("new password", type="password")
-                    password2 = st.text_input(
-                        "confirm password", type="password", disabled=True
-                    )
-
-                # password2 = st.text_input("Confirm password", type="password")
-                # if password1 != password2:
-                #     st.error("Passwords do not match. Please try again.")
-                submitted = st.form_submit_button("Submit", type="primary")
-                if submitted:
-                    pass
+        # password2 = st.text_input("Confirm password", type="password")
+        # if password1 != password2:
+        #     st.error("Passwords do not match. Please try again.")
+        submitted = st.form_submit_button("Submit", type="primary")
+        if submitted:
+            pass
